6e92945ef70e4288b45bfea22fa83bccdc2b708c
SublimeYe <sublimeye.ua@gmail.com>
2014-01-25 15:34:04 +0200

    updates

--- server/index.js
+++ server/index.js
@@ -2,10 +2,34 @@ var express = require('express');
 var config = require('./config');
 
 var app = express();
-app.configure(function() {
-	app.use(express.static(__dirname + config.buildDir));
+app.configure(function () {
+    app.use(express.static(__dirname + config.buildDir));
 });
 
-express().use(express.vhost(config.vhost, app)).listen(config.port);
+var isPortTaken = function (port, fn) {
+    var net = require('net');
+    var tester = net.createServer()
+        .once('error', function (err) {
+            if (err.code != 'EADDRINUSE') {
+                return fn(err)
+            }
+            fn(null, true);
+            return true;
+        })
+        .once('listening', function () {
+            tester.once('close', function () {
+                fn(null, false)
+            })
+                .close()
+        })
+        .listen(port)
+};
 
-console.log('Server: http://'+ config.vhost + ':' + config.port);
+isPortTaken(config.port, function (res, taken) {
+    if (!taken) {
+        express().use(express.vhost(config.vhost, app)).listen(config.port);
+        console.log('Server running: http://' + config.vhost + '. (should be set in hosts file) On port: ' + config.port);
+    } else {
+        console.log('Port ' + config.port + ' is already taken by some other application; Either change that application port, or update port number in /server/config.js');
+    }
+});
